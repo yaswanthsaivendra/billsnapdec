643d1d21bc1ac7fe164258d68c79cc6c97a78a6c
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2023-02-22 15:46:40 +0530

    added notificatoins

--- billapp/urls.py
+++ billapp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('custdash/',views.dashcustomer,name='custdash'),
     path('customer-plans/',views.customer_plans,name='customer-plans'),
+    path('customer-notifications/',views.customer_notifications,name='customer-notifications'),
     path('send/',views.sending,name='send'),
     path('settings/', views.sett_ings, name='settings'),
     path('dashboard/', views.dashboard, name='dash'),
